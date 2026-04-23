cc233fbe2146818d2fae755ce6baf1b1fbeba3e4
umukundwaa <a.umukundwa1@alustudent.com>
2026-04-23 01:29:48 -0700

    feat(geo): add GeoLocationSummary DTO

--- modules/geo/src/main/java/com/motherhood/geo/application/dto/GeoLocationSummary.java
+++ modules/geo/src/main/java/com/motherhood/geo/application/dto/GeoLocationSummary.java
@@ -1,4 +1,18 @@
 package com.motherhood.geo.application.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GeoLocationSummary {
+
+    private UUID id;
+    private String sector;
+    private String cell;
+    private String village;
 }
