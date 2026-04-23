81884ab67b7d32fa7538007a6d3e884c54f7c694
umukundwaa <a.umukundwa1@alustudent.com>
2026-04-23 01:29:21 -0700

    feat(geo): add GeoLocationResponse DTO

--- modules/geo/src/main/java/com/motherhood/geo/application/dto/GeoLocationResponse.java
+++ modules/geo/src/main/java/com/motherhood/geo/application/dto/GeoLocationResponse.java
@@ -1,3 +1,22 @@
 package com.motherhood.geo.application.dto;
 
-public class GeoLocationResponse {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeoLocationResponse {
+
+    private UUID id;
+    private String province;
+    private String district;
+    private String sector;
+    private String cell;
+    private String village;
+    private Double latitude;
+    private Double longitude;
+}
