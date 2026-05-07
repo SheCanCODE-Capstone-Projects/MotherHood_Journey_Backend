42b2a24d13374ccfce258211f083a1398fe34e3a
umukundwaa <a.umukundwa1@alustudent.com>
2026-05-07 03:16:16 -0700

    feat(geo): add GeoResponse DTO

--- src/main/java/com/motherhood/journey/geo/dto/response/GeoResponse.java
+++ src/main/java/com/motherhood/journey/geo/dto/response/GeoResponse.java
@@ -1,4 +1,22 @@
 package com.motherhood.journey.geo.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GeoResponse {
+
+    private UUID id;
+    private String province;
+    private String district;
+    private String sector;
+    private String cell;
+    private String village;
+    private Double latitude;
+    private Double longitude;
 }
