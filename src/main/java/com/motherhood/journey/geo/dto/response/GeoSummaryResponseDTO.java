c8b3d1baa368e75eb13487642f6b6f8722b02cd3
umukundwaa <a.umukundwa1@alustudent.com>
2026-05-07 03:19:05 -0700

    feat(geo): add GeoSummaryResponse DTO

--- src/main/java/com/motherhood/journey/geo/dto/response/GeoSummaryResponseDTO.java
+++ src/main/java/com/motherhood/journey/geo/dto/response/GeoSummaryResponseDTO.java
@@ -1,4 +1,18 @@
 package com.motherhood.journey.geo.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GeoSummaryResponseDTO {
+
+    private UUID id;
+    private String sector;
+    private String cell;
+    private String village;
 }
